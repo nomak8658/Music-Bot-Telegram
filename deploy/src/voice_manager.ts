3150824aa9d2eb818b3d3a5c3e356207f4589312
nomak8658 <63632029+nomak8658@users.noreply.github.com>
2026-04-27 16:23:23 +0300

    feat: handle session_activated event from background Telethon init

--- deploy/src/voice_manager.ts
+++ deploy/src/voice_manager.ts
@@ -60,6 +60,10 @@ class VoiceManager extends EventEmitter {
           if (msg.event === "ready") {
             this.ready = true;
             this.emit("ready");
+          } else if (msg.event === "session_activated") {
+            // Background session init succeeded
+            logger.info(`VoiceService session activated: ${msg.name || ""}`);
+            this.emit("session_activated", msg);
           } else {
             const resolver = this.pendingResolvers.shift();
             if (resolver) resolver(msg);
